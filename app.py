42bfa614f6fdab96ea62ba6c4092a07d513e9a93
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2025-11-26 16:16:35 -0500

    Revert "move filer titles to y axis"
    This reverts commit a024fccfee603be889c95f3307a3815b04d9586e.

--- app.py
+++ app.py
@@ -74,19 +74,23 @@ def create_chart(df, width=1200, height=800,
                     
         selectors[var] = alt.selection_interval(name = var + '_brush')
         if var == 'time':
-            x = alt.X('year(' + var + '):T', bin=alt.Bin(maxbins=30), title = None)
+            x = alt.X('year(' + var + '):T', bin=alt.Bin(maxbins=30))
             type = ':T'
 
         else:
             type = ':Q'
-            x = alt.X(var + type, bin=alt.Bin(maxbins=30), title = None)
+            x = alt.X(var + type, bin=alt.Bin(maxbins=30))
 
         hists[var] = alt.Chart(df).mark_bar().encode(
             x = x,
+<<<<<<< HEAD
             y = alt.Y('count()', 
                       title = var[:3],
                       axis = alt.Axis(values = [10,100,1000]),
                       scale = alt.Scale(type = 'log')),
+=======
+            y = alt.Y('count()', title = ''),
+>>>>>>> parent of a024fcc (move filer titles to y axis)
             color = alt.condition(selectors[var],
                                  alt.Color('magnitude:Q',
                                            scale = alt.Scale(scheme = color_scheme)),
