49cb923e244a4431f0dd68a307780c86439e34b9
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2025-11-28 10:37:54 -0500

    check query parameters

--- DataLoader.py
+++ DataLoader.py
@@ -5,5 +5,13 @@ import geopandas as gpd
 #A Class for loading earthquake data from USGS API
 class DataLoader:
     def __init__(self, params: dict):
+        for key in params.keys():
+            assert key in ['starttime', 'endtime', 
+                           'minlatitude', 'maxlatitude',
+                           'minlongitude', 'maxlongitude',
+                           'mindepth', 'maxdepth',
+                           'minmagnitude', 'maxmagnitude',
+                           'minsig', 'maxsig'], "Invalid parameter"
+        
         self.params = params
         self.url = "https://earthquake.usgs.gov/fdsnws/event/1/"
