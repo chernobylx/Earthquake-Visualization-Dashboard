65a5f282e67efd522d71b60009d1ffc9dacdf8c3
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2025-11-28 08:36:31 -0500

    store df

--- DataVisualizer.py
+++ DataVisualizer.py
@@ -2,3 +2,9 @@ import pandas as pd
 import geopandas as gpd
 import altair as alt
 from vega_datasets import data
+
+
+class DataVisualizer:
+    def __init__(self, df: pd.DataFrame):
+        #set internal dataframe
+        self.df = df
