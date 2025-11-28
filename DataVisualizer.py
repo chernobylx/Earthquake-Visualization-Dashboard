8fde05fd4ac22c55cb545423807aa40a15f920bf
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2025-11-28 09:24:00 -0500

    check df col types

--- DataVisualizer.py
+++ DataVisualizer.py
@@ -10,5 +10,16 @@ class DataVisualizer:
         assert not df.empty, "Input DataFrame must not be empty"
         for col in ['lat', 'lon', 'mag', 'sig', 'depth', 'time']:
             assert col in df.columns, f"DataFrame must contain '{col}' column"
+        
+        col_types = {
+            'lat': 'float64',
+            'lon': 'float64',
+            'mag': 'float64',
+            'sig': 'int64',
+            'depth': 'float64',
+            'time': 'datetime64[ns, UTC]'
+        }
+        for col, expected_type in col_types.items():
+            assert df[col].dtype == expected_type, f"Column '{col}' must be of type {expected_type}"
         #set internal dataframe
         self.df = df
