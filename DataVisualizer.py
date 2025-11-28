a8d273e282608c6c8bf03ec6e44ed6e38da3d1f4
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2025-11-28 08:38:14 -0500

    assert df is not empty

--- DataVisualizer.py
+++ DataVisualizer.py
@@ -7,5 +7,6 @@ from vega_datasets import data
 class DataVisualizer:
     def __init__(self, df: pd.DataFrame):
         assert isinstance(df, pd.DataFrame), "Input must be a pandas DataFrame"
+        assert not df.empty, "Input DataFrame must not be empty"
         #set internal dataframe
         self.df = df
