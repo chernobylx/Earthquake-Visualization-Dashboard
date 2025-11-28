5762a62b429431da6cfc047641e2dca2bb4d4b37
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2025-11-28 10:14:39 -0500

    init

--- DataLoader.py
+++ DataLoader.py
@@ -1,3 +1,9 @@
 import requests
 from datetime import datetime, timedelta
 import geopandas as gpd
+
+#A Class for loading earthquake data from USGS API
+class DataLoader:
+    def __init__(self, params: dict):
+        self.params = params
+        self.url = "https://earthquake.usgs.gov/fdsnws/event/1/"
