ec2e2f6c6ab5266453fa5e8206f22aed931c3600
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2025-11-29 13:29:12 -0500

    move param definition to __init__;  create test_df to test the queried dataframe

--- test_loader.py
+++ test_loader.py
@@ -20,31 +20,40 @@ class TestRequestParams:
 
 
 class TestDataLoader:
-
-    #test DataLoader.count 
-    def test_count(self):
+    def __init__(self):
         #set start and endtimes
-        starttime = datetime(year=2025,month=11,day=20)
-        endtime = datetime(year=2025,month=11,day=21)
+        self.starttime = datetime(year=2025,month=11,day=20)
+        self.endtime = datetime(year=2025,month=11,day=21)
         #convert them to strings
-        start = datetime.strftime(starttime, DT_FORMAT)
-        end = datetime.strftime(endtime, DT_FORMAT)
+        self.start = datetime.strftime(starttime, DT_FORMAT)
+        self.end = datetime.strftime(endtime, DT_FORMAT)
         #construct params
-        params = RP(starttime=start, endtime=end, minmagnitude=5)
-        #validate params
-        assert params.validate()
-        dl = DataLoader(params) 
-        assert dl.count() == 6
+        self.params = RP(starttime=start, endtime=end, minmagnitude=5)
+        self.params.validate()
     
+    #test DataLoader.count 
+    def test_count(self): 
+        dl = DataLoader(self.params) 
+        assert dl.count() == 6
+
+    #test DataLoader.query
     def test_query(self):
-        starttime = datetime(year=2025,month=11,day=20)
-        endtime = datetime(year=2025,month=11,day=21)
-        #convert them to strings
-        start = datetime.strftime(starttime, DT_FORMAT)
-        end = datetime.strftime(endtime, DT_FORMAT)
-        #construct params
-        params = RP(starttime=start, endtime=end, minmagnitude=5)
-        #validate params
-        assert params.validate()
-        dl = DataLoader(params) 
+        dl = DataLoader(self.params) 
         assert len(dl.query()) == 6
+    
+    def test_df(self):
+        #test that the dataframe has the correct columns and datatypes required by the visualizer
+        assert not df.empty, "Input DataFrame must not be empty"
+        for col in ['lat', 'lon', 'mag', 'sig', 'depth', 'time']:
+            assert col in df.columns, f"DataFrame must contain '{col}' column"
+        
+        col_types = {
+            'lat': 'float64',
+            'lon': 'float64',
+            'mag': 'float64',
+            'sig': 'int64',
+            'depth': 'float64',
+            'time': 'datetime64[ns, UTC]'
+        }
+        for col, expected_type in col_types.items():
+            assert df[col].dtype == expected_type, f"Column '{col}' must be of type {expected_type}"
