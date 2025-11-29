8b002272d5d308528e73f8245fdaf406d7a401cc
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2025-11-29 17:43:47 -0500

    create daterange picker

--- pages/load.py
+++ pages/load.py
@@ -1,6 +1,7 @@
 import dash
-from dash import html, dcc, callback, Input, Output
+from dash import html, dcc, callback, Input, Output, State
 from datetime import date, timedelta
+from DataLoader import DataLoader, RequestParams
 
 dash.register_page(__name__)
 
@@ -13,6 +14,15 @@ layout = html.Div([
         initial_visible_month=date.today()-timedelta(days=30),
         end_date=date.today()+timedelta(days=1)
     ),
+    html.Button('Load', id='load_button', n_clicks = 0),
     html.Div(id='output_div')
 ])
 
+@callback(
+    Output('output_div', 'children'),
+    State('date_range', 'start_date'),
+    State('date_range', 'end_date'),
+    Input('load_button', 'n_clicks')
+)
+def update_output(start_date, end_date, n_clicks):
+    return (start_date, end_date)
