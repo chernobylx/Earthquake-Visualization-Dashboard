943912c64b6b969491ceb63b5f8ac52d70fe0144
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2025-11-29 17:27:13 -0500

    add date picker

--- pages/load.py
+++ pages/load.py
@@ -1,8 +1,16 @@
 import dash
 from dash import html, dcc, callback, Input, Output
+from datetime import date, timedelta
 
 dash.register_page(__name__)
 
 layout = html.Div([
-    html.H1('Data Loader')
+    html.H1('Data Loader'),
+    dcc.DatePickerRange(
+        id = 'date_range',
+        min_date_allowed=date(1900,1,1),
+        max_date_allowed=date.today()+timedelta(days=1),
+        initial_visible_month=date.today()-timedelta(days=30),
+        end_date=date.today()+timedelta(days=1)
+    )  
 ])
