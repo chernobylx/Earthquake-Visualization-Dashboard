c2be9e16f4de561302b424e9439a2a3eb744dea7
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2025-11-29 18:22:45 -0500

    add magnitude range slider

--- pages/load.py
+++ pages/load.py
@@ -14,7 +14,21 @@ layout = html.Div([
         initial_visible_month=date.today()-timedelta(days=30),
         end_date=date.today()+timedelta(days=1)
     ),
+    html.Div(
+        [dcc.RangeSlider(
+            min=0,
+            max=10,
+            step=.1,
+            value=[6,9.1],
+            marks = None,
+            tooltip = {"placement": "bottom", "always_visible": True},
+            id = 'mag_range',
+        )],
+        style = {'width': '200px'},
+    ),
+
     html.Button('Load', id='load_button', n_clicks = 0),
+
     html.Div(id='output_div')
 ])
 
@@ -22,15 +36,19 @@ layout = html.Div([
     Output('output_div', 'children'),
     State('date_range', 'start_date'),
     State('date_range', 'end_date'),
-    Input('load_button', 'n_clicks')
+    State('mag_range', 'value'),
+    Input('load_button', 'n_clicks'),
+    prevent_initial_call=True
 )
-def update_output(start_date, end_date, n_clicks):
+def update_output(start_date, end_date, 
+                  magrange,
+                  n_clicks):
     format = "%Y-%m-%d"
     start_time = datetime.strptime(start_date, format)
     start_time = datetime.strftime(start_time, DT_FORMAT)
     end_time = datetime.strptime(end_date, format)
     end_time = datetime.strftime(end_time, DT_FORMAT)
-    
+
     params = RequestParams(starttime=start_time, endtime=end_time, minmagnitude=5)
     dl = DataLoader(params)
     return dl.count()
