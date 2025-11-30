700ef3bd0bf5faf268a89aa188d41d9cdb16ffaa
Jonathan Chernoch <33642531+chernobylx@users.noreply.github.com>
2025-11-30 12:18:57 -0500

    migrate functions from app.py; update app.py

--- DataVisualizer.py
+++ DataVisualizer.py
@@ -11,9 +11,203 @@ class DataVisualizer:
         assert not df.empty, "Input DataFrame must not be empty"
         for col in COL_TYPES.keys():
             assert col in df.columns, f"DataFrame must contain '{col}' column"
-        
-        
+
+
         for col, expected_type in COL_TYPES.items():
             assert df[col].dtype == expected_type, f"Column '{col}' must be of type {expected_type}"
         #set internal dataframe
         self.df = df
+
+    def create_heatmap(self, filters, width, height, x_var='time', y_var='depth', color_var='max(magnitude)'):
+        day = 24*60*60*1000
+
+        if x_var == 'time':
+            X = alt.X('time:T',
+                      axis = alt.Axis(format = '%Y'),
+                      bin = alt.BinParams(step = 365 * day),
+                      title = 'Date')
+            X_tooltip = alt.Tooltip('year(time):T', title='Time')
+        else:
+            X = alt.X(x_var+':Q',
+                      axis = alt.Axis(),
+                      bin = alt.BinParams(),
+                      title = x_var.capitalize())
+            X_tooltip = alt.Tooltip(x_var+':Q', title=x_var.capitalize())
+
+        reversed_y = (y_var == 'depth')
+        if y_var == 'time':
+            Y = alt.Y('time:T',
+                      axis = alt.Axis(format = '%Y'),
+                      bin = alt.BinParams(step = 365 * day),
+                      title = 'Date')
+            Y_tooltip = alt.Tooltip('year(time):T', title='Time')
+        else:
+            Y = alt.Y(y_var+':Q',
+                      axis = alt.Axis(),
+                      scale = alt.Scale(reverse = reversed_y),
+                      bin = alt.BinParams(),
+                      title = y_var.capitalize())
+            Y_tooltip = alt.Tooltip(y_var+':Q', title=y_var.capitalize())
+
+        Color = alt.Color(color_var,
+                          scale = alt.Scale(scheme = 'magma'))
+
+
+        chart = alt.Chart(self.df).mark_rect().encode(
+            x = X,
+            y = Y,
+            color = Color,
+            tooltip = [X_tooltip,
+                       Y_tooltip,
+                       alt.Tooltip(color_var+':Q', title = color_var.capitalize())]
+        ).transform_filter(
+            *filters
+        )
+        return chart
+
+    def create_hists_selectors(self, filter_vars, filter_width, filter_height, color_scheme='magma'):
+        hists = {}
+        selectors = {}
+        for var in filter_vars:
+
+            selectors[var] = alt.selection_interval(name = var + '_brush')
+            if var == 'time':
+                x = alt.X('time:T',
+                        timeUnit = 'year',
+                        axis = alt.Axis(format = '%Y'),
+                        bin=True,
+                        title = None)
+                type = ':T'
+
+            else:
+                type = ':Q'
+                x = alt.X(var + type, bin=alt.Bin(maxbins=30), title = None)
+
+            hists[var] = alt.Chart(self.df).mark_bar().encode(
+                x = x,
+                y = alt.Y('count()', title = var[:4]),
+                color = alt.condition(selectors[var],
+                                    alt.Color('magnitude:Q',
+                                            scale = alt.Scale(scheme = color_scheme)),
+                                    alt.value('lightgrey')),
+                order = alt.Order(var+type, sort='ascending')
+                ).properties(
+                    width = filter_width,
+                    height = filter_height,
+                ).add_params(
+                    selectors[var]
+                )
+        return hists, selectors
+
+    def create_map(self, map_fill, map_stroke, map_width, map_height, Projection):
+        topo = alt.topo_feature(data.world_110m.url, 'countries')
+        earth = alt.Chart(topo).mark_geoshape(
+            fill = map_fill,
+            stroke = map_stroke
+        ).properties(
+            width = map_width,
+            height = map_height,
+            projection = Projection
+        )
+
+        graticule = alt.Chart(alt.graticule()).mark_geoshape().properties(projection = Projection)
+
+        earth += graticule
+        return earth
+
+    def create_chart(self, width=1200, height=800,
+                     projection ='equalEarth', phi = 0, theta = 0, scale = 100,
+                     map_fill = 'darkgrey', map_stroke = 'lightgrey',
+                     color_var = 'significance', color_scheme = 'magma',
+                     opacity_var = 'magnitude',
+                     size_var = 'magnitude', size_range = [10, 200],
+                     filter_vars = ['time', 'magnitude', 'significance', 'depth', 'longitude', 'latitude'],
+                     heatmap_x = 'time',
+                     heatmap_y = 'depth'):
+        map_width = .6 * width
+        map_height = .8 * height
+
+        filter_width = map_width
+        filter_height = (height - map_height) / len(filter_vars)
+
+        heatmap__width = width - map_width
+        heatmap_height = map_height
+
+        rotation = [phi, theta, 0]
+        Projection = alt.Projection(type = projection,
+                                    rotate=rotation,
+                                    scale = scale,
+                                    translate = [map_width/2, map_height/2])
+
+        if color_var == 'time':
+            color_var += ':T'
+        else:
+            color_var += ':Q'
+
+        if size_var == 'time':
+            size_var += ':T'
+        else:
+            size_var += ':Q'
+
+        if opacity_var == 'time':
+            opacity_var += ':T'
+        else:
+            opacity_var += ':Q'
+
+        ColorScale = alt.Scale(scheme = color_scheme, domain = [self.df[color_var[:-2]].min(), self.df[color_var[:-2]].max()])
+        ColorLegend = alt.Legend(title = color_var)
+        Color = alt.Color(color_var, scale = ColorScale, legend=ColorLegend)
+
+        SizeScale = alt.Scale(range=size_range, domain = [self.df[size_var[:-2]].min(), self.df[size_var[:-2]].max()])
+        SizeLegend = alt.Legend(title = size_var)
+        Size = alt.Size(size_var, scale=SizeScale, legend=SizeLegend)
+
+        OpacityScale = alt.Scale(range = [0.1, 1], domain = [self.df[opacity_var[:-2]].min(), self.df[opacity_var[:-2]].max()])
+        OpacityLegend = alt.Legend(title = opacity_var)
+        Opacity = alt.Opacity(opacity_var, scale=OpacityScale, legend=OpacityLegend)
+
+        hists, selectors = self.create_hists_selectors(filter_vars, filter_width, filter_height, color_scheme=color_scheme)
+
+        earth = self.create_map(map_fill, map_stroke, map_width, map_height, Projection)
+
+        brush = alt.selection_interval(name = "brush")
+        quakes = alt.Chart(self.df).mark_circle().encode(
+            longitude = 'longitude:Q',
+            latitude = 'latitude:Q',
+            size = Size,
+            opacity= Opacity,
+            color = alt.condition(brush,
+                                Color,
+                                alt.value('lightgrey')),
+            order = alt.Order('time:T', sort='ascending'),
+            tooltip = [
+                alt.Tooltip('location:N', title='Location'),
+                alt.Tooltip('magnitude:Q', title='Magnitude'),
+                alt.Tooltip('depth:Q', title='Depth (km)'),
+                alt.Tooltip('time:T', title='Time')
+            ]
+        ).properties(
+            projection = Projection
+        ).add_params(
+            brush
+        ).transform_filter(
+            *selectors.values()
+        )
+
+        filters = [selector for selector in selectors.values()]
+        filters.append(brush)
+        heatmap = self.create_heatmap(filters = filters,
+                                 x_var = heatmap_x,
+                                 y_var = heatmap_y,
+                                 width = heatmap__width,
+                                 height = heatmap_height,
+                                 color_var = 'max(magnitude)')
+
+        earth+=quakes
+
+        for hist in hists.values():
+            earth &= hist
+
+        earth |= heatmap
+        return earth
+
--- app.py
+++ app.py
@@ -1,231 +1,23 @@
-#import requests
 import pandas as pd
-#from io import StringIO
-#import ipywidgets as widgets
-#from IPython.display import display, clear_output
-#from datetime import datetime, timedelta
 import altair as alt
-from vega_datasets import data
-#from ipywidgets import interact, interactive, fixed, interact_manual, IntSlider, FloatSlider, DatePicker
-#import geopandas as gpd
-#import json
 import dash_vega_components as dvc
 from dash import Dash, Input, Output, callback, dcc, html
+from DataVisualizer import DataVisualizer
+
 alt.data_transformers.disable_max_rows()
 
 
-df = pd.read_csv('test.csv')
+df = pd.read_csv('data/test.csv')
 
 def preprocess(df):
     df.set_index('id', inplace=True)
     df['time'] = pd.to_datetime(df['time'], format = 'ISO8601')
     df = df.sample(5000, random_state=42)
     return df
+
 df = preprocess(df)
+visualizer = DataVisualizer(df)
 
-def create_heatmap(df, filters,width, height, x_var='time', y_var='depth', color_var='max(magnitude)', ):
-        day = 24*60*60*1000
-
-        if x_var == 'time':
-            X = alt.X('time:T',
-                      axis = alt.Axis(format = '%Y'),
-                      bin = alt.BinParams(step = 365 * day),
-                      title = 'Date')
-            X_tooltip = alt.Tooltip('year(time):T', title='Time')
-        else:
-            X = alt.X(x_var+':Q',
-                      axis = alt.Axis(),
-                      bin = alt.BinParams(),
-                      title = x_var.capitalize())
-            X_tooltip = alt.Tooltip(x_var+':Q', title=x_var.capitalize())
-
-        reversed_y = (y_var == 'depth')
-        if y_var == 'time':
-            Y = alt.Y('time:T',
-                      axis = alt.Axis(format = '%Y'),
-                      bin = alt.BinParams(step = 365 * day),
-                      title = 'Date')
-            Y_tooltip = alt.Tooltip('year(time):T', title='Time')
-        else:
-            Y = alt.Y(y_var+':Q',
-                      axis = alt.Axis(),
-                      scale = alt.Scale(reverse = reversed_y),
-                      bin = alt.BinParams(),
-                      title = y_var.capitalize())
-            Y_tooltip = alt.Tooltip(y_var+':Q', title=y_var.capitalize())
-        
-        Color = alt.Color(color_var,
-                          scale = alt.Scale(scheme = 'magma'))
-            
-            
-        chart = alt.Chart(df).mark_rect().encode(
-            x = X,
-            y = Y,
-            color = Color,
-            tooltip = [X_tooltip,
-                       Y_tooltip,
-                       alt.Tooltip(color_var+':Q', title = color_var.capitalize())]
-        ).transform_filter(
-            *filters
-        )
-        return chart
-def create_hists_selectors(df, filter_vars, filter_width, filter_height, color_scheme='magma'):
-    
-    hists = {}
-    selectors = {}
-    for var in filter_vars:
-                    
-        selectors[var] = alt.selection_interval(name = var + '_brush')
-        if var == 'time':
-            x = alt.X('time:T',
-                    timeUnit = 'year',
-                    axis = alt.Axis(format = '%Y'), 
-                    bin=True, 
-                    title = None)
-            type = ':T'
-
-        else:
-            type = ':Q'
-            x = alt.X(var + type, bin=alt.Bin(maxbins=30), title = None)
-
-        hists[var] = alt.Chart(df).mark_bar().encode(
-            x = x,
-            y = alt.Y('count()', title = var[:4]),
-            color = alt.condition(selectors[var],
-                                alt.Color('magnitude:Q',
-                                        scale = alt.Scale(scheme = color_scheme)),
-                                alt.value('lightgrey')),
-            order = alt.Order(var+type, sort='ascending')
-            ).properties(
-                width = filter_width,
-                height = filter_height,
-            ).add_params(
-                selectors[var]
-            )
-    return hists, selectors
-def create_map(map_fill, map_stroke, map_width, map_height, Projection):
-    topo = alt.topo_feature(data.world_110m.url, 'countries')
-    earth = alt.Chart(topo).mark_geoshape(
-        fill = map_fill,
-        stroke = map_stroke
-    ).properties(
-        width = map_width,
-        height = map_height,
-        projection = Projection
-    )
-
-    graticule = alt.Chart(alt.graticule()).mark_geoshape().properties(projection = Projection)
-
-    earth += graticule
-    return earth
-
-def create_chart(df, width=1200, height=800, 
-                 projection ='equalEarth', phi = 0, theta = 0, scale = 100,
-                 map_fill = 'darkgrey', map_stroke = 'lightgrey',
-                 color_var = 'significance', color_scheme = 'magma',
-                 opacity_var = 'magnitude',
-                 size_var = 'magnitude', size_range = [10, 200],
-                 filter_vars = ['time', 'magnitude', 'significance', 'depth', 'longitude', 'latitude'],
-                 heatmap_x = 'time',
-                 heatmap_y = 'depth'):
-    map_width = .6 * width
-    map_height = .8 * height
-
-    filter_width = map_width
-    filter_height = (height - map_height) / len(filter_vars)
-
-    heatmap__width = width - map_width
-    heatmap_height = map_height
-
-    rotation = [phi, theta, 0]
-    Projection = alt.Projection(type = projection, 
-                                rotate=rotation, 
-                                scale = scale, 
-                                translate = [map_width/2, map_height/2])
-
-    if color_var == 'time':
-        color_var += ':T'
-    else:
-        color_var += ':Q'
-    
-    if size_var == 'time':
-        size_var += ':T'
-    else:
-        size_var += ':Q'
-
-    if opacity_var == 'time':
-        opacity_var += ':T'
-    else:
-        opacity_var += ':Q'
-
-    ColorScale = alt.Scale(scheme = color_scheme, domain = [df[color_var[:-2]].min(), df[color_var[:-2]].max()])
-    ColorLegend = alt.Legend(title = color_var)
-    Color = alt.Color(color_var, scale = ColorScale, legend=ColorLegend)
-
-    SizeScale = alt.Scale(range=size_range, domain = [df[size_var[:-2]].min(), df[size_var[:-2]].max()])
-    SizeLegend = alt.Legend(title = size_var)
-    Size = alt.Size(size_var, scale=SizeScale, legend=SizeLegend)
-
-    OpacityScale = alt.Scale(range = [0.1, 1], domain = [df[opacity_var[:-2]].min(), df[opacity_var[:-2]].max()])
-    OpacityLegend = alt.Legend(title = opacity_var)
-    Opacity = alt.Opacity(opacity_var, scale=OpacityScale, legend=OpacityLegend)
-
-
-    
-    hists, selectors = create_hists_selectors(df, filter_vars, filter_width, filter_height, color_scheme=color_scheme)
-
-
-
-    
-
-
-
-    earth = create_map(map_fill, map_stroke, map_width, map_height, Projection)
-
-    brush = alt.selection_interval(name = "brush")
-    quakes = alt.Chart(df).mark_circle().encode(
-        longitude = 'longitude:Q',
-        latitude = 'latitude:Q',
-        size = Size,
-        opacity= Opacity,
-        color = alt.condition(brush,
-                            Color,
-                            alt.value('lightgrey')),
-        order = alt.Order('time:T', sort='ascending'),
-        tooltip = [
-            alt.Tooltip('location:N', title='Location'),
-            alt.Tooltip('magnitude:Q', title='Magnitude'),
-            alt.Tooltip('depth:Q', title='Depth (km)'),
-            alt.Tooltip('time:T', title='Time')
-        ]
-    ).properties(
-        projection = Projection
-    ).add_params(
-        brush
-    ).transform_filter(
-        *selectors.values()
-    )
-
-    
-    
-    filters = [selector for selector in selectors.values()]
-    filters.append(brush)
-    heatmap = create_heatmap(df, filters = filters,
-                             x_var = heatmap_x,
-                             y_var = heatmap_y,
-                             width = heatmap__width,
-                             height = heatmap_height,
-                             color_var = 'max(magnitude)')
-
-
-    earth+=quakes
-
-    for hist in hists.values():
-        earth &= hist
-
-    earth |= heatmap
-    return earth
-#
 app = Dash()
 app.layout = html.Div([
     html.H1('Title'),
@@ -342,30 +134,31 @@ app.layout = html.Div([
     Input('heatmap_x', 'value'),
     Input('heatmap_y', 'value'),
 )
-def update_output(proj_dd, phi, theta, scale, map_fill, map_stroke, background, 
-                  size_var, color_var, opacity_var, 
-                  filter_vars, 
+def update_output(proj_dd, phi, theta, scale, map_fill, map_stroke, background,
+                  size_var, color_var, opacity_var,
+                  filter_vars,
                   heatmap_x, heatmap_y):
-    chart_spec = create_chart(df, 
-                              projection=proj_dd, 
-                              phi=phi, theta=theta, 
-                              scale = scale, 
-                              map_fill=map_fill, 
-                              map_stroke = map_stroke,
-                              size_var=size_var,
-                              color_var=color_var,
-                              opacity_var=opacity_var,
-                              filter_vars=filter_vars,
-                              heatmap_x=heatmap_x,
-                              heatmap_y=heatmap_y,
-                              width = 1200,
-                              height = 500,
-                              ).properties(
-                                  background = background).to_dict()
+    chart_spec = visualizer.create_chart(
+        projection=proj_dd,
+        phi=phi, theta=theta,
+        scale = scale,
+        map_fill=map_fill,
+        map_stroke = map_stroke,
+        size_var=size_var,
+        color_var=color_var,
+        opacity_var=opacity_var,
+        filter_vars=filter_vars,
+        heatmap_x=heatmap_x,
+        heatmap_y=heatmap_y,
+        width = 1200,
+        height = 500,
+    ).properties(
+        background = background).to_dict()
     return dvc.Vega(
         id='map',
         signalsToObserve=['brush'],
         opt={"renderer": 'svg', 'actions': False},
         spec=chart_spec
     )
+
 app.run(debug = True)
